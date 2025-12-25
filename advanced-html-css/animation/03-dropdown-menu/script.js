c76dbc6ed85e9bd57e8cc14151a0c3abfb87f31e
Julia-1439 <195843068+Julia-1439@users.noreply.github.com>
2025-12-24 22:36:37 -0800

    complete exercise 03 dropdown-menu

--- advanced-html-css/animation/03-dropdown-menu/script.js
+++ advanced-html-css/animation/03-dropdown-menu/script.js
@@ -3,7 +3,7 @@ const menuTitle = document.querySelector(".menu-title");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
 menuTitle.addEventListener("click", (e) => {
-  if (e.target === e.currentTarget) {
+  if (e.target === e.currentTarget) { /* why need this? i guess don't want event bubbling on anything on top of the menuTitle div to inadvertedly trigger the dropdown. */
     dropdownMenu.classList.toggle("visible");
   }  
 })
